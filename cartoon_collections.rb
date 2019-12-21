2b9ab8aebd6dd1ea61458362c5cdb81ed866818e
Terren Culberson <culbersonterren@yahoo.com>
2019-12-21 16:21:31 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -17,9 +17,7 @@ def summon_captain_planet(array)
   nu_array
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
-end
+
 
 def find_the_cheese# code an argument here
   # the array below is here to help

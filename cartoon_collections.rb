c2b043f0f80c2d78b2c1910b61d41245be2702a6
Terren Culberson <culbersonterren@yahoo.com>
2019-12-21 16:30:03 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -27,4 +27,9 @@ def long_planeteer_calls(array)
   end 
 end
 
-
+def find_the_cheese2(array, array2)
+  cheese_types = array2
+    array.find do |type|
+    cheese_types.include?(type)
+  end 
+end
